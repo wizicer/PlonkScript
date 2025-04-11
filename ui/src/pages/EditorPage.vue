fbc0e23b2abb4430e755d854f8b3de033bfbf21b
Liang, Shuang (Icer) <liangshuang@sutu.tech>
2025-04-11 23:05:23 +0800

    update default code to latest syntax

--- ui/src/pages/EditorPage.vue
+++ ui/src/pages/EditorPage.vue
@@ -92,51 +92,41 @@ monaco.languages.register({ id: 'plonkscript' });
 monaco.languages.setMonarchTokensProvider('plonkscript', language);
 monaco.editor.defineTheme('plonkscript', theme);
 
-const mainCode = `
-# k: 4
+const mainCode = `# k: 4
 # in1: 1
 # in2: 1
 
-gate add(a, b, c, s) {
-    s <| a + b - c;
-}
-
-region first_row(a, b, c, s, in1, in2) {
+region first_row(a, b, c, in1, in2) {
     a[0] <== in1;
     b[0] <== in2;
     c[0] <== a[0] + b[0];
-    s[0] <-- enable;
 
     [b[0], c[0]]
 }
 
-region next_row(a, b, c, s, last_b, last_c) {
+region next_row(a, b, c, last_b, last_c) {
     a[0] <== last_b;
     b[0] <== last_c;
     c[0] <== a[0] + b[0];
-    s[0] <-- enable;
 
     c[0]
 }
 
 let N = 10;
 
-public input in1;
-public input in2;
-public output out;
+pub input in1;
+pub input in2;
+pub output out;
 
-private advice a;
-private advice b;
-private advice c;
+col advice a;
+col advice b;
+col advice c;
 
-public selector s;
-
-add(a, b, c, s);
-let fr = first_row(a, b, c, s, in1, in2);
+let fr = first_row(a, b, c, in1, in2);
 let last_b = fr[0];
 let last_c = fr[1];
 for i in 1..N {
-    let c = next_row(a, b, c, s, last_b, last_c);
+    let c = next_row(a, b, c, last_b, last_c);
     last_b = last_c;
     last_c = c;
 }
